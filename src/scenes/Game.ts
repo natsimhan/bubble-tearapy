1f0605c5beaf9e1fa5efef1891ab03d6f1776ecd
nathan <jonathan@ouebsson.fr>
2025-01-25 10:23:30 +0100

    NEW: RNG + exemple
    - Mise en place d'un RNG simple, qui prend un seed mais qui en génère un
      si rien n'est donné.
    - Exemple simple intégré dans la scène Game pour l'usage et son
      initialisation, il devra être transporté aux élements qui en ont
      besoin depuis la scène.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,40 @@
-import { Scene } from 'phaser';
+import {Scene} from 'phaser';
+import Rng from '../domain/Rng.ts';
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Scene {
+  private rng: Rng;
 
-    constructor ()
-    {
-        super('Game');
-    }
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+  msg_text: Phaser.GameObjects.Text;
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+  constructor() {
+    super('Game');
+    this.rng = new Rng('BubbleTearapy');
+  }
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x00ff00);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+    this.background = this.add.image(512, 384, 'background');
+    this.background.setAlpha(0.5);
 
-        this.input.once('pointerdown', () => {
+    this.msg_text = this.add.text(512, 384, 'Seed : ' + this.rng.getSeed() + '\n Roll the D20 !', {
+      fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+      stroke: '#000000', strokeThickness: 8,
+      align: 'center'
+    });
+    this.msg_text.setOrigin(0.5);
 
-            this.scene.start('GameOver');
+    this.input.on('pointerup', () => {
+      const diceResult = this.rng.rollADice();
+      this.msg_text.setText('Seed : ' + this.rng.getSeed() + '\n' + diceResult);
 
-        });
-    }
+      if (diceResult === 1) {
+        this.scene.start('GameOver');
+      }
+
+    });
+  }
 }
